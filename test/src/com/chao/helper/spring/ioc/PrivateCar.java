730d4989767772e865e4e5688ab75011a669877e
quchao <42692108@qq.com>
2017-02-03 16:13:16 +0800

    JAVA基础知识   Spring-ioc

---
+++ test/src/com/chao/helper/spring/ioc/PrivateCar.java
@@ -0,0 +1,15 @@
+package com.chao.helper.spring.ioc;
+
+/**
+ * Created by think on 2017/2/3.
+ */
+public class PrivateCar {
+    //①private成员变量：使用传统的类实例调用方式，只能在本类中访问
+    private String color;
+
+    //②protected方法：使用传统的类实例调用方式，只能在子类和本包中访问
+    protected void drive() {
+
+        System.out.println("drive private car! the color is:" + color);
+    }
+}
