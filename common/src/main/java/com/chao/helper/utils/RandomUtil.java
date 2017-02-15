09a2cd41bc47d128917278efa14bf8ad73cf64ed
quchao <42692108@qq.com>
2017-02-15 10:46:08 +0800

    生成随机数工具类

---
+++ common/src/main/java/com/chao/helper/utils/RandomUtil.java
@@ -0,0 +1,27 @@
+package com.chao.helper.utils;
+
+import java.util.Random;
+
+/**
+ * Created by think on 2017/2/15.
+ */
+public class RandomUtil {
+
+    /**
+     * 返回长度为【strLength】的随机数，在前面补0
+     */
+    public static String getFixLenthString(int strLength) {
+
+        Random rm = new Random();
+
+        // 获得随机数
+        double pross = (1 + rm.nextDouble()) * Math.pow(10, strLength);
+
+        // 将获得的获得随机数转化为字符串
+        String fixLenthString = String.valueOf(pross);
+
+        // 返回固定的长度的随机数
+        return fixLenthString.substring(1, strLength + 1);
+    }
+
+}
