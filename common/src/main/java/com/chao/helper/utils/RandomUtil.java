f332c7da8f5d6b7c010c639917301ee690a5617a
quchao <42692108@qq.com>
2017-03-13 14:34:20 +0800

    java 随机码

--- common/src/main/java/com/chao/helper/utils/RandomUtil.java
+++ common/src/main/java/com/chao/helper/utils/RandomUtil.java
@@ -1,5 +1,8 @@
 package com.chao.helper.utils;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Random;
 
 /**
@@ -22,6 +25,30 @@ public class RandomUtil {
 
         // 返回固定的长度的随机数
         return fixLenthString.substring(1, strLength + 1);
+    }
+
+    /**
+     * 返回长度为【length】的随机码
+     */
+    private static String getRandomString(int length) { //length表示生成字符串的长度
+//        String base = "abcdefghijklmnopqrstuvwxyz0123456789";
+        String base = "0123456789";
+        Random random = new Random();
+        StringBuffer sb = new StringBuffer();
+        for (int i = 0; i < length; i++) {
+            int number = random.nextInt(base.length());
+            sb.append(base.charAt(number));
+        }
+        return sb.toString();
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(getRandomString(16));
+        DateFormat df = new SimpleDateFormat("yyMMddHH");
+        System.out.println(df.format(new Date())+getRandomString(8));
+        System.out.println(System.nanoTime());
+        System.out.println(System.currentTimeMillis());
+
     }
 
 }
